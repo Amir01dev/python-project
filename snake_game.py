efbeb349158dec275ee3d9212e3c4ca08b15cd09
Amirreza Jamshidi <amir010101developer@gmail.com>
2023-11-17 16:40:00 +0330

    Update snake game

--- snake_game.py
+++ snake_game.py
@@ -1,13 +1,17 @@
 from tkinter import *
-
+# ----------function game-------------
+def restart_button():
+    pass
 # ----------variables game-------------
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPACE_WIDTH = 30
+SPACE_SIZE = 30
+SLOWNESS = 200
 SNAKE_COLOR = "yellow"
 FOOD_COLOR = "red"
 BACKGROUND_COLOR = "black"
 score = 0
+direction = "down"
 
 # ----------GUI Game-------------
 window = Tk()
@@ -21,5 +25,20 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, width=GAME_WIDTH, height=GAME_HEIGHT)
 canvas.pack()
+
+restart = Button(window, text="Restart", fg="red", command=restart_button)
+restart.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
