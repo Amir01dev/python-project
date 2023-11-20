db11af6143455e7e3043cba1c225bd07717ad2a0
Amirreza Jamshidi <amir010101developer@gmail.com>
2023-11-20 23:41:21 +0330

    Update snake game

--- snake_game.py
+++ snake_game.py
@@ -20,7 +20,25 @@ class Food:
         y = randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
         self.coordinates = [x, y]
         canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
+
+
+def next_turn(snake, food):
+    x, y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+    snake.coordinates.index(0, [x, y])
+
 def restart_button():
+    pass
+
+def change_direction():
     pass
 # ----------variables game-------------
 GAME_WIDTH = 700
@@ -62,7 +80,13 @@ y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind("<Left>", lambda event: change_direction("left"))
+window.bind("<Right>", lambda event: change_direction("right"))
+window.bind("<Up>", lambda event: change_direction("up"))
+window.bind("<Down>", lambda event: change_direction("down"))
+
 snake = Snake()
 food = Food()
+next_turn(snake, food)
 
 window.mainloop()
